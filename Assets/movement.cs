a051184b4ce8ed34db47578cf25a153f983e5cb0
bren <brendan.freeman@comcast.net>
2020-02-14 20:09:33 -0500

    init

---
+++ Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class movement : MonoBehaviour
+{
+    public int speed = 0;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // get input data from keyboard or controller
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        // update player position based on input
+        Vector3 position = transform.position;
+        position.x += moveHorizontal * speed * Time.deltaTime;
+        position.z += moveVertical * speed * Time.deltaTime;
+        //position = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * position;
+        transform.position = position;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y - Input.GetAxis("Fire1") * speed * 13 * Time.deltaTime, 0);
+    }
+}
